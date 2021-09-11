b8270620fee71d4fa6f3f4cc6b94c854777d5373
matsuokayuri <88954075+yuri-matsuoka@users.noreply.github.com>
2021-09-11 19:05:21 +0900

    Update and rename introduce.rb to kadai-introduce.rb

--- introduce.rb
+++
@@ -1,5 +0,0 @@
-myouzi="松岡"
-namae="侑里"
-nenrei=20
-simei=myouzi+namae+"です。"
-puts simei+nenrei.to_s + "歳です。"
---
+++ kadai-introduce.rb
@@ -0,0 +1,4 @@
+myouzi="松岡"
+namae="侑里"
+nenrei=20
+puts myouzi+namae+"です。"+nenrei.to_s + "歳です。"
